263040b5c02d8b1a5ac096c448a7f664007bbc95
Chris <55300770+pizzatree@users.noreply.github.com>
2020-04-18 12:28:14 -0500

    added enemy spawner

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private float spawnRate = 3f,
+                  spawnRateChange = 0.2f,
+                  spawnRateFloor = 1f,
+                  spawnDistanceFromPlayer = 15f;
+
+    private float currentSpawnRate;
+    private float spawnTimer;
+
+    private Transform player => GameObject.FindGameObjectWithTag("Player").transform;
+
+    [SerializeField]
+    private GameObject[] Enemies;
+
+    public void LowerSpawnRate() 
+        => spawnRate -= (spawnRate > spawnRateFloor) ? spawnRateChange : 0;
+
+    private void Start()
+    {
+        currentSpawnRate = spawnRate;
+        spawnTimer = spawnRate;
+    }
+
+    private void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+
+        if (spawnTimer <= 0)
+            HandleSpawn();
+    }
+
+    private void HandleSpawn()
+    {
+        var desiredSpawn = (Vector2)player.position + Random.insideUnitCircle.normalized * spawnDistanceFromPlayer; 
+        var hits = Physics2D.BoxCastAll(desiredSpawn, Vector2.one, 0, Vector2.one);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider.CompareTag("No Spawn"))
+                return;
+        }
+
+        Instantiate(Enemies[Random.Range(0, Enemies.Length)], desiredSpawn, Quaternion.identity);
+        spawnTimer = currentSpawnRate;
+    }
+}
