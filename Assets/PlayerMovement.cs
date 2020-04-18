6f537e6e115a62ce56888cc7d41c40cca1e5ded6
Chris <55300770+pizzatree@users.noreply.github.com>
2020-04-17 22:34:19 -0500

    movement + toilet guy

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f,
+                  sprintMultiplier = 2f,
+                  maxSprintTime = 3f;
+
+    private float remainingSprintTime;
+    private bool ableToSprint = true;
+    private Rigidbody2D rb => GetComponent<Rigidbody2D>();
+
+    private void Start() => remainingSprintTime = maxSprintTime;
+    private void FixedUpdate() => HandleMovement();
+
+    private void HandleMovement()
+    {
+        remainingSprintTime += Time.fixedDeltaTime;
+        remainingSprintTime = Mathf.Clamp(remainingSprintTime, -.1f, remainingSprintTime);
+
+        var moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        
+        if(Input.GetKey(KeyCode.LeftShift) && ableToSprint) // Handle Sprint
+        {
+            moveInput *= sprintMultiplier;
+            remainingSprintTime -= 2 * Time.fixedDeltaTime;
+            if (remainingSprintTime <= 0f)
+            {
+                ableToSprint = false;
+                Invoke("ResetSprint", maxSprintTime);
+            }
+        }
+
+        rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
+    }
+
+    private void ResetSprint() => ableToSprint = true;
+}
