469d5f97ccd4ed9749fc92e2f98ecfb0779fcdce
Chris <55300770+pizzatree@users.noreply.github.com>
2020-04-18 10:23:00 -0500

    added lose condition

---
+++ Assets/Scripts/ToiletGuyHealth.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletGuyHealth : Health
+{
+    protected override void Die()
+    {
+        Debug.Log("Game over");
+    }
+}
