1d4f4ef6421b86dbcf3e2e3304285eedf2b1535b
Chris <55300770+pizzatree@users.noreply.github.com>
2020-04-18 11:47:41 -0500

    ground planes

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : Health
+{
+    SpriteRenderer sprite => GetComponentInChildren<SpriteRenderer>();
+
+    public override void TakeDamage(float amount)
+    {
+        base.TakeDamage(amount);
+
+        var spriteColor = Color.Lerp(Color.black, Color.white, currentHealth / MaxHealth);
+        sprite.color = spriteColor;
+    }
+
+    protected override void Die()
+    {
+        Debug.Log("Game over");
+    }
+}
