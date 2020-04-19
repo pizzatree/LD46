80d85c8f9435bb831b40eb5e00b0d4a1a9e3505d
Chris <55300770+pizzatree@users.noreply.github.com>
2020-04-19 00:15:24 -0500

    added "doors"

--- Assets/Scripts/WallHealth.cs
+++ Assets/Scripts/WallHealth.cs
@@ -7,12 +7,17 @@ public class WallHealth : Health
 {
     private SpriteRenderer sprite => GetComponent<SpriteRenderer>();
 
+    [SerializeField]
+    private bool door = false;
     public override void TakeDamage(float amount)
     {
         base.TakeDamage(amount);
 
-        var wallColor = Color.Lerp(Color.red, Color.green, currentHealth / MaxHealth);
-        sprite.color = wallColor;
+        if (!door)
+        {
+            var wallColor = Color.Lerp(Color.red, Color.green, currentHealth / MaxHealth);
+            sprite.color = wallColor;
+        }
     }
 
     // override Die and add debris explosion
