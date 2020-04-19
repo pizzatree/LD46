9be6d0e8849e4c16bc2070f89c9f319685821f98
Chris <55300770+pizzatree@users.noreply.github.com>
2020-04-19 01:22:12 -0500

    added ammo & tp pickups
    don't do anything yet

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    [SerializeField]
+    private PickupType pickupType;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            // add to inventory
+            Destroy(gameObject);
+        }
+    }
+}
+
+public enum PickupType
+{
+    Toiletpaper,
+    Money,
+    Ammo
+}
